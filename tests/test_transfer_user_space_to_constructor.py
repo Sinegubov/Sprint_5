79008c1f288d5c7960cd506e7805db66677d66c1
Pavel Sinegubov <sineguboff@gmail.com>
2024-05-02 01:20:39 +0700

    fix tests

--- tests/test_transfer_user_space_to_constructor.py
+++ tests/test_transfer_user_space_to_constructor.py
@@ -7,11 +7,11 @@ class TestTransferToConstructor:
     def test_successful_transfer_from_userspace_to_constructor_click_logo(self, wb, get_login_driver):
         wb.find_element(*AuthLoc.lk_btn).click()
         wb.find_element(*UserSpace.logo_btn).click()
-        WebDriverWait(wb, 10).until(expected_conditions.visibility_of_element_located(UserSpace.logo_btn))
+        WebDriverWait(wb, 10).until(expected_conditions.visibility_of_element_located(MainPageLoc.h1_header))
         assert wb.find_element(*MainPageLoc.h1_header).text == "Соберите бургер"
 
     def test_successful_transfer_from_userspace_to_constructor_click_constructor(self, wb, get_login_driver):
         wb.find_element(*AuthLoc.lk_btn).click()
         wb.find_element(*UserSpace.constructor_btn).click()
-        WebDriverWait(wb, 10).until(expected_conditions.visibility_of_element_located(UserSpace.logo_btn))
+        WebDriverWait(wb, 10).until(expected_conditions.visibility_of_element_located(MainPageLoc.h1_header))
         assert wb.find_element(*MainPageLoc.h1_header).text == "Соберите бургер"
